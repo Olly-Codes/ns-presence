ba4fc9f9e2964aad64612510d12ab092b3e9ef5b
Olly <ollycodesdev@gmail.com>
2026-04-29 03:32:54 +0200

    Add gameDropDown component for list of games to choose from

---
+++ src/components/gameDropDown.jsx
@@ -0,0 +1,25 @@
+function gameDropDown({ games, selectedGame, onSelect }) {
+    function handleChange(e) {
+        const game = games.find((game) => game.id === e.target.value)
+        onSelect(game || null)
+    }
+
+    return (
+        <div>
+            <label>Select a Game</label>
+            <select
+                value = {selectedGame?.id || ""}
+                onChange={handleChange}
+            >
+                <option value = "" disabled>Choose a game</option>
+                {games.map((game) => {
+                    <option key={game.id} value={game.id}>
+                        {game.name}
+                    </option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default gameDropDown
