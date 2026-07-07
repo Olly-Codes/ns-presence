67625a84a1590a11f7fcce30bbafc2007713e788
Olly <ollycodesdev@gmail.com>
2026-07-07 19:11:06 +0200

    feat: add set game presence function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,52 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+use std::sync::Mutex;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+struct DiscordState(Mutex<Option<DiscordIpcClient>>);
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_game_presence (
+    state: tauri::State<DiscordState>,
+    game_title: String, 
+    large_image: String, 
+    small_image: String
+) -> Result<(), String> {
+    let mut guard = state.0.lock().unwrap();
+
+    if guard.is_none() {
+        let mut client = DiscordIpcClient::new("1524026976292307205");
+        client.connect().map_err(|e| e.to_string())?;
+        *guard = Some(client);
+    }
+
+    let client = guard.as_mut().unwrap();
+
+    let start_time: i64 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map_err(|e| e.to_string())?
+        .as_secs() as i64;
+
+    let timestamps = activity::Timestamps::new().start(start_time);
+    let assets = activity::Assets::new()
+        .large_image(&large_image)
+        .large_text(&game_title)
+        .small_image(&small_image)
+        .small_text("Nintendo Switch");
+
+    client.set_activity(activity::Activity::new()
+        .details(&game_title)
+        .timestamps(timestamps)
+        .assets(assets)).map_err(|e| e.to_string())?;
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(DiscordState(Mutex::new(None)))
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![set_game_presence])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
