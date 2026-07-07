965bd7843ad8534d512d412aae320893ca35b6c8
Olly <ollycodesdev@gmail.com>
2026-07-08 00:45:53 +0200

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header-content">
+            <h1>NS Presence</h1>
+            <p>Discord Rich Presence</p>
+        </header>
+    );
+};
+
+export default Header;
